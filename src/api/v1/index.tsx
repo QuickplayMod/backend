a33b702940087060ebbe9a4c75ad049e76904bbc
Erik <7330807+robere2@users.noreply.github.com>
2024-08-13 23:25:48 -0400

    feat: Add initial JSON schema for games API

--- src/api/v1/index.tsx
+++ src/api/v1/index.tsx
@@ -1,10 +1,26 @@
 import {Hono} from 'hono'
+import games from './games.json'
+import gamesSchema from './games.schema.json'
 
 const app = new Hono()
 
 app.get('/health', (c) => {
     return c.json({
         online: true
+    })
+})
+
+app.get('/games', (c) => {
+    return c.json(games)
+})
+app.get('/gamesSchema', (c) => {
+    return c.json(gamesSchema)
+})
+
+app.get('*', (c) => {
+    c.status(404);
+    return c.json({
+        error: "Not found"
     })
 })
 
